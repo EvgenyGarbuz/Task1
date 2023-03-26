b99a61d38ea2ec317c05321e7c8cc73246eaec4e
evgenygarbuz <evgeny.garbuz.99@mail.ru>
2023-03-26 22:05:40 +0600

    Task

---
+++ Seminar1/Program.cs
@@ -0,0 +1,46 @@
+﻿// int number1 = 5;
+// double number2 = 12.5;
+
+// string word = "my word";
+
+// bool check = false;
+
+// int num1 = 5;
+// int num2 =10;
+// Console.WriteLine("My number is " + num1 + " and " + num2 + " and it is good!");
+// Console.WriteLine($"My number are {num1} and {num2} and it is good!");
+
+// Console.Write("Input a numder: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Your number is " + num);
+
+
+
+// Console.Write("Input a first numder");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input a second numder ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quat2 = num2 * num2;
+
+// if(num1 == quat2)
+// {
+//   Console.WriteLine("yes ");
+// }
+// else
+// {
+//   Console.WriteLine("no");
+// }
+
+// Console.Write("Input positive numder");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int current = -num;
+
+// while(current <= num);
+// {
+//     Console.WriteLine(current + " ");
+//     current++;
+// }
+
